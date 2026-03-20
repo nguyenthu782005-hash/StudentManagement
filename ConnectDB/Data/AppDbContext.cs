f19c6ad6b6a85039531534cd92680476106e97e0
pc <pc@THU>
2026-03-20 16:06:25 +0700

    Add project files.

---
+++ ConnectDB/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ConnectDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectDB.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+}
