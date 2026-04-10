6370e49a67cca82ded688eee94b7c4a0f4391a59
Nguyen Minh Thu <nguyenthu782005@gmail.com>
2026-04-10 15:12:49 +0700

    Chuyển đổi sang PostgreSQL và bật Swagger ở Production

--- ConnectDB/Program.cs
+++ ConnectDB/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ConnectDB.Models;
 
 namespace ConnectDB
@@ -9,21 +9,39 @@ namespace ConnectDB
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            // Cấu hình Database: Ưu tiên PostgreSQL nếu có biến môi trường ConnectionStrings:PostgresConnection
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            var postgresConnection = builder.Configuration.GetConnectionString("PostgresConnection");
 
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                if (!string.IsNullOrEmpty(postgresConnection))
+                {
+                    options.UseNpgsql(postgresConnection);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
+
+            // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            // Bật Swagger cho cả môi trường Production để dễ dàng kiểm tra trực tuyến
             app.UseSwagger();
             app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            // Trang chủ mặc định
+            app.MapGet("/", () => "API is running! Visit /swagger to test the endpoints.");
 
             app.MapControllers();
 
